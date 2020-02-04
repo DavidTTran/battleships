8ab54b8894d90133b46239e2b66b818c56234dc8
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-03 18:06:25 -0700

    Add base methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,23 @@
 class Ship
 
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
 
   def initialize(name_parameter, length_parameter)
     @name = name_parameter
     @length = length_parameter
+    @health = @length
+  end
+
+  def sunk?
+    if  @health == @ength
+      true
+    else
+      false
+    end
+  end
+
+  def hit
+    @health - 1
   end
 
 end
